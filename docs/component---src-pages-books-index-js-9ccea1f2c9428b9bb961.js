445f92f9093ca691c9237ab164f9afc37a62d859
Nicolas Segarra <sopkope@gmail.com>
2019-11-04 23:57:00 +0800

    build

---
+++ docs/component---src-pages-books-index-js-9ccea1f2c9428b9bb961.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{273:function(e,o,n){"use strict";n.r(o);var t=n(1),r=n.n(t),a=n(32);o.default=function(){return r.a.createElement(a.Redirect,{noThrow:!0,to:"/books/proud-to-be-me-part-1"})}}}]);
+//# sourceMappingURL=component---src-pages-books-index-js-9ccea1f2c9428b9bb961.js.map
